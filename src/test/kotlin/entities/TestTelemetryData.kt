211d0ea6ba153d348da7f2e89254cb21c62cea65
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-02-09 11:54:22 +0100

    chore: add test for telemetry data

---
+++ src/test/kotlin/entities/TestTelemetryData.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entities
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+
+class TestTelemetryData : StringSpec({
+
+    "Create saturation with a percentage lower than 0 to the saturation throw an invalid argument exception" {
+        shouldThrow<RuntimeException> {
+            Saturation(-5)
+        }
+    }
+
+    "Create saturation with a percentage greater than 100 to the saturation throw an invalid argument exception" {
+        shouldThrow<RuntimeException> {
+            Saturation(101)
+        }
+    }
+})
