bfdc4e2603aed20a7523df928762c89d262cee8a
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-02-09 11:00:46 +0100

    feat: create telemetrydata class

--- src/main/kotlin/entities/TelemetryData.kt
+++ src/main/kotlin/entities/TelemetryData.kt
@@ -10,64 +10,74 @@ package entities
 
 /**
  * Object that represents the telemetry data.
+ * @param bodyTemperature the temperature of the body.
+ * @param saturation the saturation of the blood.
+ * @param respirationRate the respiration rate of a person.
+ * @param heartBeat the heartbeat of a person.
+ * @param bloodPressure the pressure of the blood. 
  */
-object TelemetryData {
+class TelemetryData(
+    val bodyTemperature: BodyTemperature,
+    val saturation: Saturation,
+    val respirationRate: RespirationRate,
+    val heartBeat: HeartBeat,
+    val bloodPressure: BloodPressure
+)
+
+/**
+ * Represents the body temperature.
+ * @param temperature the value of temperature.
+ * @param temperatureUnit the used unit of measurement of the temperature.
+ */
+data class BodyTemperature(val temperature: Double, val temperatureUnit: UnitOfMeasurement.TemperatureUnit)
+
+/**
+ * Represents the blood oxygen level.
+ * @param percentage the percentage of saturation.
+ */
+data class Saturation(val percentage: Int)
+
+/**
+ * Represents how many times a person breathes in a minute.
+ * @param breathPerMinute the number of breath in one minute.
+ */
+data class RespirationRate(val breathPerMinute: Int)
+
+/**
+ * Represents the blood pressure of a person.
+ * @param diastolicBloodPressure the diastolic blood pressure.
+ * @param systolicBloodPressure the systolic blood pressure.
+ * @param bloodPressureUnit the blood pressure unit.
+ */
+data class BloodPressure(
+    val diastolicBloodPressure: Int,
+    val systolicBloodPressure: Int,
+    val bloodPressureUnit: UnitOfMeasurement.BloodPressureUnit
+)
+
+/**
+ * Represents the hearth beat of a person.
+ * @param beatPerMinute the beat per minute.
+ */
+data class HeartBeat(val beatPerMinute: Int)
+
+/**
+ * Object that contains enum for unit of measurement.
+ */
+object UnitOfMeasurement {
 
     /**
-     * Represents the body temperature.
-     * @param temperature the value of temperature.
-     * @param temperatureUnit the used unit of measurement of the temperature.
+     * Enum class for blood pressure unit.
      */
-    data class BodyTemperature(val temperature: Double, val temperatureUnit: UnitOfMeasurement.TemperatureUnit)
+    enum class BloodPressureUnit {
+        MMHG
+    }
 
     /**
-     * Represents the blood oxygen level.
-     * @param percentage the percentage of saturation.
+     * Enum class for temperature unit.
      */
-    data class Saturation(val percentage: Int)
-
-    /**
-     * Represents how many times a person breathes in a minute.
-     * @param breathPerMinute the number of breath in one minute.
-     */
-    data class RespirationRate(val breathPerMinute: Int)
-
-    /**
-     * Represents the blood pressure of a person.
-     * @param diastolicBloodPressure the diastolic blood pressure.
-     * @param systolicBloodPressure the systolic blood pressure.
-     * @param bloodPressureUnit the blood pressure unit.
-     */
-    data class BloodPressure(
-        val diastolicBloodPressure: Int,
-        val systolicBloodPressure: Int,
-        val bloodPressureUnit: UnitOfMeasurement.BloodPressureUnit
-    )
-
-    /**
-     * Represents the hearth beat of a person.
-     * @param beatPerMinute the beat per minute.
-     */
-    data class HeartBeat(val beatPerMinute: Int)
-
-    /**
-     * Object that contains enum for unit of measurement.
-     */
-    object UnitOfMeasurement {
-
-        /**
-         * Enum class for blood pressure unit.
-         */
-        enum class BloodPressureUnit {
-            MMHG
-        }
-
-        /**
-         * Enum class for temperature unit.
-         */
-        enum class TemperatureUnit {
-            CELSIUS,
-            FAHRENHEIT
-        }
+    enum class TemperatureUnit {
+        CELSIUS,
+        FAHRENHEIT
     }
 }
