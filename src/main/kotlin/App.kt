9e0022f8fb23b7c15b67ab312a9b037042de00be
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-02-11 15:10:51 +0100

    chore: add simple simulation of data received from a third-party system about medical instrument

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,8 @@
+
+import application.MedicalInstrumentController
+import infrastructure.DigitalTwinMedicalInstrumentManager
+import infrastructure.MedicalInstrumentDataReceiver
+
 /*
  * Copyright (c) 2023. Smart Operating Block
  *
@@ -10,5 +15,26 @@
  * Template for kotlin projects.
  */
 fun main() {
-    println("Hello World from Kotlin Template")
+    val medicalInstrumentManager = DigitalTwinMedicalInstrumentManager()
+    val medicalInstrumentController = MedicalInstrumentController(medicalInstrumentManager)
+    val medicalInstrumentDataReceiver = MedicalInstrumentDataReceiver(medicalInstrumentController)
+    val telemetrySystemData = """
+        {
+        	"telemetrySystemID": "TelemetrySystem",
+        	"patientID" : "patient2", 
+        	"beatPerMinute" : 30.0, 
+        	"bloodPressure" : {
+        		"diastolicBloodPressure" : 1000, 
+        		"systolicBloodPressure" : 2000, 
+        		"bloodPressureUnit" : mmhg
+        	}, 
+        	"breathPerMinute" : 20.0, 
+        	"saturation" : 20, 
+        	"bodyTemperature" : {
+        		"temperature" : 36.5,
+        		"temperatureUnit" : "celsius"
+        	}
+        }
+    """.trimIndent()
+    medicalInstrumentDataReceiver.receiveTelemetrySystemData(telemetrySystemData)
 }
