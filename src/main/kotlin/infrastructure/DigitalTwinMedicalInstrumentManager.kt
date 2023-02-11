dc546e58fd8856017835d905303a5eb6bb29e944
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-02-11 15:10:09 +0100

    feat: create manager for medical instrument to pulish data on azure digital twin plaftorm

---
+++ src/main/kotlin/infrastructure/DigitalTwinMedicalInstrumentManager.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package infrastructure
+import application.MedicalInstrumentManager
+import application.presenter.serializer.JsonPatchSerializer
+import com.azure.core.models.JsonPatchDocument
+import com.azure.digitaltwins.core.DigitalTwinsClient
+import com.azure.digitaltwins.core.DigitalTwinsClientBuilder
+import com.azure.identity.ClientSecretCredentialBuilder
+import entities.MedicalInstrument
+import entities.TelemetrySystem
+
+/**
+ * The Azure endpoint.
+ */
+const val ENDPOINT = "https://digital-twin-layer.api.neu.digitaltwins.azure.net"
+
+/**
+ * The Azure tenant id.
+ */
+const val TENANT_ID = "8e97c84a-c23e-4463-8faf-d9a974450bc1"
+
+/**
+ * The Azure client id.
+ */
+const val CLIENT_ID = "52e143e0-1993-436f-ac31-8ba5787445e4"
+
+/**
+ * The Azure client secret.
+ */
+const val CLIENT_SECRET = "zuu8Q~0JA2NnKpnXEDMyew4WdBcw.XIjYWwK8b1R"
+
+/**
+ * Manager that manage the update of the digital twin on Azure digital twin platform.
+ */
+class DigitalTwinMedicalInstrumentManager : MedicalInstrumentManager<JsonPatchDocument> {
+
+    private val client: DigitalTwinsClient = DigitalTwinsClientBuilder()
+        .credential(
+            ClientSecretCredentialBuilder()
+                .tenantId(TENANT_ID)
+                .clientId(CLIENT_ID)
+                .clientSecret(CLIENT_SECRET)
+                .build()
+        )
+        .endpoint(ENDPOINT)
+        .buildClient()
+
+    /**
+     * Updates the digital twin.
+     * @param medicalInstrumentID the id of the digital twin to update.
+     * @param command the json with the instruction to update the digital twin.
+     */
+    override fun update(medicalInstrumentID: String, medicalInstrument: MedicalInstrument) {
+        val command = createCommand(medicalInstrument)
+        println("command = $command")
+        client.updateDigitalTwin(medicalInstrumentID, command)
+    }
+
+    /**
+     * Given the medical instrument, creates the command serializing the content.
+     * @param medicalInstrument the medical instrument.
+     */
+    private fun createCommand(medicalInstrument: MedicalInstrument): JsonPatchDocument {
+        val serializer = JsonPatchSerializer.TelemetrySystemJSONPatchSerializer()
+        when (medicalInstrument) {
+            is TelemetrySystem -> return serializer.serialize(medicalInstrument)
+            else -> throw UnsupportedOperationException()
+        }
+    }
+}
