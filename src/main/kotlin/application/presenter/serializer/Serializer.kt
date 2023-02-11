48fa92f5e38bb12a5e0d04ee57e564aa913055f5
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-02-11 15:04:52 +0100

    chore: create interface for a serializer

---
+++ src/main/kotlin/application/presenter/serializer/Serializer.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.serializer
+
+/**
+ * Interface of a generic data serializer.
+ */
+interface Serializer<I, O> {
+
+    /**
+     * Serializes an entity.
+     */
+    fun serialize(entity: I): O
+}
