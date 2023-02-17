1dcbd0fca31e26f3d4f531b3ccceedd4c830ba41
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-02-17 16:59:29 +0100

    chore: set ktor to receive information from a telemetry system

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -9,15 +9,30 @@
 import application.MedicalInstrumentController
 import infrastructure.DigitalTwinMedicalInstrumentManager
 import infrastructure.MedicalInstrumentDataReceiver
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.request.receiveText
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
 
 /**
- * Template for kotlin projects.
+ * Main function.
  */
-fun main() {
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
 
+/**
+ * Manages http requests to get data from a medical instrument.
+ */
+fun Application.module() {
     val medicalInstrumentManager = DigitalTwinMedicalInstrumentManager()
     val medicalInstrumentController = MedicalInstrumentController(medicalInstrumentManager)
     val medicalInstrumentDataReceiver = MedicalInstrumentDataReceiver(medicalInstrumentController)
-    val thirdPartySystemSimulator = ThirdPartySystemSimulator(medicalInstrumentDataReceiver)
-    thirdPartySystemSimulator.startSimulator()
+
+    routing {
+        post("/telemetrySystem") {
+            medicalInstrumentDataReceiver.receiveTelemetrySystemData(call.receiveText())
+        }
+    }
 }
